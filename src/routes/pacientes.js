b7f41f23c6c2a02598d0513e43317f88d941701f
Walter Casarino <wcasarino@gmail.com>
2019-05-17 12:44:11 -0300

    02 Trabajo

--- src/routes/pacientes.js
+++ src/routes/pacientes.js
@@ -5,11 +5,15 @@ const { isAuthenticated } = require('../helpers/auth');
 
 router.get('/paciente/seek', isAuthenticated, (req, res) => {
     //res.send('OK LOCO');
+    //let viewModel = { DNI: String, pacientes: {}, protocolo: {} };
     res.render('pacientes/seek-paciente');
 });
 
 router.post('/paciente/seek', isAuthenticated, async (req, res) => {
     const { DNI } = req.body;
+    console.log(req.body)
+    let viewModel = { DNI: String, pacientes: {}, protocolo: {} };
+    viewModel.DNI = DNI;
     const errors = [];
     if (!DNI) {
         errors.push({text: 'Por favor ingrese un DNI'});
@@ -21,14 +25,27 @@ router.post('/paciente/seek', isAuthenticated, async (req, res) => {
         const pacientes = await Protocolo.find({"Paciente.DNI": DNI})
         .limit(15)
         .sort({ Fecha: -1 });
-        //console.log(pacientes, DNI);
-        res.render('pacientes/seek-paciente', { pacientes });
+        viewModel.pacientes = pacientes;
+        //console.log(viewModel);
+        res.render('pacientes/seek-paciente', viewModel );
     }
 });
 
-router.get('/protocolo/view/:id', isAuthenticated, async (req, res) => {
-    const protocolo = await Protocolo.findById(req.params.id);
-    res.render('partials/protocolo', {protocolo});
+router.post('/protocolo/seek/:id', isAuthenticated, async (req, res) => {
+    let viewModel = { DNI: String, pacientes: {}, protocolo: {} };
+    //console.log(req.params.id);
+    const protocolo = await Protocolo.findById(req.params.id); 
+    //console.log(protocolo);
+    //console.log(protocolo.Paciente.DNI);
+    const pacientes = await Protocolo.find({"Paciente.DNI": protocolo.Paciente.DNI})
+        .limit(15)
+        .sort({ Fecha: -1 });
+    viewModel.pacientes = pacientes;
+    viewModel.protocolo = protocolo;
+    viewModel.DNI = protocolo.Paciente.DNI;
+    //console.log(viewModel);
+    res.render('pacientes/seek-paciente', viewModel );
+    //res.send('ok');
 });
 
 
